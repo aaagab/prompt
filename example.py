dd0dc232d1a1ef40c07051a5010e1585512a3b2c
gabaaa <gabaaa.dev@gmail.com>
2018-12-06 13:44:50 -0600

    added new version message, added get_path function, improved modules import

--- example.py
+++ example.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
+# author: Gabriel Auger
+# version: 1.0.0-draft-1543082164
+# name: prompt
+# license: MIT
+
+
+import sys, os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 import prompt as prompt
 import modules.message.message as msg
+del sys.path[0:2]
 
 msg.info("Basic Input, type empty to see the results")
 print(prompt.prompt("Input your name"))
@@ -30,4 +40,4 @@ print(prompt.prompt_multiple(
     )))
 print(prompt.prompt_boolean("Are you busy"))
 print(prompt.prompt_boolean("Are you busy",'n'))
-prompt.get_path("Choose path (autocomplete with tab)")
+print(prompt.get_path("Choose path (autocomplete with tab)"))
