12804b181bef909b99c0008c09cbde675f9fbb93
gabaaa <gabaaa.dev@gmail.com>
2020-06-19 07:06:47 -0500

    added field hidden, added function pause

--- __init__.py
+++ __init__.py
@@ -5,12 +5,13 @@
 # license: MIT
 __version__ = "2.0.2"
 
-from .dev.prompt import prompt, prompt_multiple, prompt_boolean, get_path
+from .dev.prompt import prompt, prompt_multiple, prompt_boolean, get_path, pause
 __all__ = [
     "prompt",
     "prompt_multiple",
     "prompt_boolean",
     "get_path",
+    "pause",
 ]
 
 from .gpkgs import message as msg
